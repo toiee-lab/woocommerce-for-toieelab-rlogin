c78ae16c7a428191d4daf6ff7470c0476c69efd3
T. Kameda <kame@hokuken.com>
2018-12-26 16:56:40 +0900

    初期設定

---
+++ woocommerce-for-toieelab-rlogin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Woocommerce For Toieelab Rlogin
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     woocommerce-for-toieelab-rlogin
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Woocommerce_For_Toieelab_Rlogin
+ */
